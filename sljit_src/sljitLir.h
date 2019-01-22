7acea4753f98b733330be641c6f2f0bd39a6747c
Michael Munday <mike.munday@ibm.com>
2019-01-22 05:39:44 -0500

    Delete unused field.
    We always need to allocate a register save area at the moment.

--- sljit_src/sljitLir.h
+++ sljit_src/sljitLir.h
@@ -443,11 +443,6 @@ struct sljit_compiler {
 	sljit_sw cache_argw;
 #endif
 
-#if (defined SLJIT_CONFIG_S390X && SLJIT_CONFIG_S390X)
-	/* Need to allocate register save area to make calls. */
-	sljit_s32 have_save_area;
-#endif
-
 #if (defined SLJIT_VERBOSE && SLJIT_VERBOSE)
 	FILE* verbose;
 #endif
